8bb170c6e61225f31579581f0db449fc634ea3fb
Bruno Alves <brunoalves@mbp-de-bruno.home>
2020-11-30 16:09:07 -0300

    rm db.sqlite3

---
+++ clean_all.sh
@@ -0,0 +1,6 @@
+heroku run python3 manage.py migrate account zero
+heroku run python3 manage.py migrate admin zero
+heroku run python3 manage.py migrate auth zero
+heroku run python3 manage.py migrate authtoken zero
+heroku run python3 manage.py migrate AppSolvePoscomp zero --fake
+heroku run rm db.sqlite3
