bc61c27e7ae6ab6ad0b1d68d1a7bfa6aea2ce537
Bruno Alves <brunoalves@mbp-de-bruno.home>
2020-11-30 10:56:03 -0300

    fixing tag max value

--- AppSolvePoscomp/migrations/0001_initial.py
+++ AppSolvePoscomp/migrations/0001_initial.py
@@ -39,7 +39,7 @@ class Migration(migrations.Migration):
             name='Tag',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nome', models.CharField(max_length=30, unique=True)),
+                ('nome', models.CharField(max_length=100, unique=True)),
             ],
         ),
         migrations.CreateModel(
